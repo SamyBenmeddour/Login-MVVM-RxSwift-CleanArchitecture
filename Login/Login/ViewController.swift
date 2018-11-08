a8f8e2ecf7a48c46a0831e197393649f2258872e
Samy <samy.benmeddour@uppli.fr>
2018-11-08 12:12:26 +0100

    create first app module

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Samy on 08/11/2018.
+//  Copyright © 2018 Benmeddour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
